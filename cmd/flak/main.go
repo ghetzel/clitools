fe6196a44424402fd074f8e2f125b7c31102f71d
Gary Hetzel <its@gary.cool>
2019-11-25 22:31:22 -0500

    flak: first cut ready

--- cmd/flak/main.go
+++ cmd/flak/main.go
@@ -5,7 +5,9 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"os"
+	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/ghetzel/cli"
@@ -14,19 +16,30 @@ import (
 	"github.com/ghetzel/go-stockutil/fileutil"
 	"github.com/ghetzel/go-stockutil/log"
 	"github.com/ghetzel/go-stockutil/rxutil"
+	"github.com/ghetzel/go-stockutil/sliceutil"
 	"github.com/ghetzel/go-stockutil/stringutil"
 )
 
 type sshResults struct {
-	Status      int       `json:"status"`
-	Stdout      []string  `json:"stdout"`
-	Stderr      []string  `json:"stderr"`
-	StartedAt   time.Time `json:"started_at"`
-	CompletedAt time.Time `json:"completed_at"`
+	Hostname    string    `json:"hostname"`
+	Error       error     `json:"error,omitempty"`
+	Status      int       `json:"status,omitempty"`
+	Stdout      []string  `json:"stdout,omitempty"`
+	Stderr      []string  `json:"stderr,omitempty"`
+	StartedAt   time.Time `json:"started_at,omitempty"`
+	CompletedAt time.Time `json:"completed_at,omitempty"`
 }
 
 func (self *sshResults) Duration() time.Duration {
 	return self.CompletedAt.Sub(self.StartedAt)
+}
+
+func (self *sshResults) String() string {
+	if self.Error != nil {
+		return self.Error.Error()
+	} else {
+		return fmt.Sprintf("exited status %d in %v", self.Status, self.Duration().Round(time.Millisecond))
+	}
 }
 
 func main() {
@@ -44,7 +57,7 @@ func main() {
 		cli.StringFlag{
 			Name:   `log-level, L`,
 			Usage:  `Level of log output verbosity`,
-			Value:  `info`,
+			Value:  `notice`,
 			EnvVar: `LOGLEVEL`,
 		},
 		cli.StringFlag{
@@ -62,6 +75,19 @@ func main() {
 			Usage: `Specify the name of the "ssh" binary to use for copying files.`,
 			Value: `ssh`,
 		},
+		cli.StringFlag{
+			Name:  `ssh-config-file, F`,
+			Usage: `Specify the SSH configuration file to use.`,
+		},
+		cli.DurationFlag{
+			Name:  `connect-timeout, t`,
+			Usage: `Specify the connection timeout for each SSH connection.`,
+			Value: 10 * time.Second,
+		},
+		cli.StringSliceFlag{
+			Name:  `ssh-option, o`,
+			Usage: `Specify an SSH command line option in the form "-o Key=Value"`,
+		},
 	}
 
 	app.Before = func(c *cli.Context) error {
@@ -70,9 +96,66 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) {
+		if c.NArg() == 0 {
+			log.Fatalf("Must specify a command or @filename to run")
+			return
+		}
+
 		if hosts, err := parseHosts(c); err == nil {
+			var wg sync.WaitGroup
+			var script string
+			var multiline bool
+
+			if scriptfile := c.Args().First(); strings.HasPrefix(scriptfile, `@`) {
+				scriptfile = strings.TrimPrefix(scriptfile, `@`)
+
+				if data, err := fileutil.ReadAll(scriptfile); err == nil {
+					script = string(data)
+					multiline = true
+				} else {
+					log.Fatalf("failed to read %q: %v", scriptfile, err)
+					return
+				}
+			} else {
+				script = strings.Join(c.Args(), ` `)
+			}
+
+			script = strings.TrimSpace(script)
+
+			if script == `` {
+				log.Fatalf("Must specify a command or @filename to run")
+				return
+			}
+
+			sort.Strings(hosts)
+			hosts = sliceutil.UniqueStrings(hosts)
+
+			resultchan := make(chan *sshResults, len(hosts))
+			results := make([]*sshResults, 0)
+
 			for _, host := range hosts {
-				log.Noticef("HOST: %v", host)
+				wg.Add(1)
+				go sshexec(&wg, resultchan, c, host, nil, script, multiline)
+			}
+
+			go func() {
+				for result := range resultchan {
+					results = append(results, result)
+				}
+			}()
+
+			wg.Wait()
+
+			for _, result := range results {
+				lvl := log.INFO
+
+				if result.Error != nil {
+					lvl = log.ERROR
+				} else if result.Status != 0 {
+					lvl = log.WARNING
+				}
+
+				log.Logf(lvl, "[res|%s] %v", result.Hostname, result)
 			}
 		} else {
 			log.Fatal(err)
@@ -117,59 +200,109 @@ func parseHosts(c *cli.Context) ([]string, error) {
 	return hosts, nil
 }
 
-func sshexec(c *cli.Context, hostname string, env map[string]interface{}, script string) (*sshResults, error) {
+func sshexec(
+	wg *sync.WaitGroup,
+	reschan chan *sshResults,
+	c *cli.Context,
+	hostname string,
+	env map[string]interface{},
+	script string,
+	multiline bool,
+) {
+	results := new(sshResults)
+
+	defer func() {
+		reschan <- results
+		wg.Done()
+	}()
+
+	var sshTo *executil.Cmd
+
 	scp := c.String(`scp-bin`)
 	ssh := c.String(`ssh-bin`)
 	flags := make([]string, 0)
 	hostname, port := stringutil.SplitPair(hostname, `:`)
+	remoteFile := ``
 
 	if port == `` {
 		port = `22`
 	}
 
-	remoteFile := fmt.Sprintf("flak-%d-%d", time.Now().UnixNano(), rand.Intn(65536))
+	if multiline {
+		remoteFile = fmt.Sprintf("flak-%d-%d", time.Now().UnixNano(), rand.Intn(65536))
 
-	// have the script remove itself as the final command
-	script += fmt.Sprintf("\nret=$?; rm -f '%s'; exit $ret\n", remoteFile)
+		// have the script remove itself as the final command
+		script += fmt.Sprintf("\nret=$?; rm -f '%s'; exit $ret\n", remoteFile)
+	}
 
-	flags = append(flags, `-o`, `Port=`+port)
+	flags = append(flags, `-q`, `-o`, `Port=`+port)
+	flags = append(flags, `-q`, `-o`, `BatchMode=yes`)
 
-	results := new(sshResults)
-	results.StartedAt = time.Now()
+	if cf := c.String(`ssh-config-file`); cf != `` {
+		flags = append(flags, `-F`, fileutil.MustExpandUser(cf))
+	}
 
-	if filename, err := fileutil.WriteTempFile(script, ``); err == nil {
-		scpTo := executil.Command(scp, append(flags, filename, fmt.Sprintf("%s:%s", hostname, remoteFile))...)
+	if ct := c.Duration(`connect-timeout`).Round(time.Second); ct > 0 {
+		flags = append(flags, `-o`, fmt.Sprintf("ConnectTimeout=%d", ct/time.Second))
+	}
 
-		for k, v := range env {
-			scpTo.SetEnv(k, v)
+	for _, pair := range c.StringSlice(`ssh-option`) {
+		pair = strings.TrimSpace(pair)
+
+		if pair != `` {
+			flags = append(flags, `-o`, pair)
 		}
+	}
 
-		scpTo.OnStdout = cmdlog(`scp`, nil)
-		scpTo.OnStderr = cmdlog(`scp`, nil)
+	results.StartedAt = time.Now()
+	results.Hostname = hostname
 
-		if err := scpTo.Run(); err == nil {
-			sshTo := executil.Command(ssh, append(flags, hostname, remoteFile)...)
+	if multiline {
+		if filename, err := fileutil.WriteTempFile(script, ``); err == nil {
+			scpTo := executil.Command(scp, append(flags, filename, fmt.Sprintf("%s:%s", hostname, remoteFile))...)
 
 			for k, v := range env {
-				sshTo.SetEnv(k, v)
+				scpTo.SetEnv(k, v)
 			}
 
-			sshTo.OnStdout = cmdlog(`ssh`, results)
-			sshTo.OnStderr = cmdlog(`ssh`, results)
+			tag := fmt.Sprintf("scp|%s", hostname)
+			scpTo.OnStdout = cmdlog(tag, nil)
+			scpTo.OnStderr = cmdlog(tag, nil)
 
-			err := sshTo.Run()
-			status := sshTo.WaitStatus()
+			log.Debugf("[%s] exec: %s", tag, strings.Join(scpTo.Args, ` `))
 
-			results.Status = status.ExitCode
-			results.CompletedAt = time.Now()
+			if err := scpTo.Run(); err == nil {
 
-			return results, err
+			} else {
+				results.Error = fmt.Errorf("script scp: %v", err)
+				return
+			}
 		} else {
-			return nil, fmt.Errorf("script scp: %v", err)
+			results.Error = fmt.Errorf("failed to write script file: %v", err)
+			return
 		}
+
+		sshTo = executil.Command(ssh, append(flags, hostname, `./`+remoteFile)...)
 	} else {
-		return nil, fmt.Errorf("failed to write script file: %v", err)
+		sshTo = executil.Command(ssh, append(flags, hostname, script)...)
 	}
+
+	for k, v := range env {
+		sshTo.SetEnv(k, v)
+	}
+
+	tag := fmt.Sprintf("scp|%s", hostname)
+	sshTo.OnStdout = cmdlog(tag, results)
+	sshTo.OnStderr = cmdlog(tag, results)
+
+	log.Debugf("[%s] exec: %s", tag, strings.Join(sshTo.Args, ` `))
+
+	results.Error = sshTo.Run()
+	status := sshTo.WaitStatus()
+
+	results.Status = status.ExitCode
+	results.CompletedAt = time.Now()
+	return
 }
 
 func cmdlog(tag string, results *sshResults) executil.OutputLineFunc {
@@ -186,13 +319,13 @@ func cmdlog(tag string, results *sshResults) executil.OutputLineFunc {
 			lvl = log.DEBUG
 		}
 
-		log.Logf(lvl, "[%s] %s", tag, line)
-
 		if results != nil {
 			if serr {
 				results.Stderr = append(results.Stderr, line)
+				log.Logf(lvl, "[%s] %s", tag, line)
 			} else {
 				results.Stdout = append(results.Stderr, line)
+				fmt.Printf("%s|%s\n", tag, line)
 			}
 		}
 	}
